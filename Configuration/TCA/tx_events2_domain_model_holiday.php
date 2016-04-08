ff8d6b22a5281f6a289b4072b1612333ed26a82c
Stefan Froemken <froemken@gmail.com>
2016-04-08 19:14:11 +0200

    [FEATURE] Now you can add holidays on root page

---
+++ Configuration/TCA/tx_events2_domain_model_holiday.php
@@ -0,0 +1,49 @@
+<?php
+return array(
+    'ctrl' => array(
+        'title' => 'LLL:EXT:events2/Resources/Private/Language/locallang_db.xlf:tx_events2_domain_model_holiday',
+        'label' => 'title',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'rootLevel' => 1,
+        'dividers2tabs' => true,
+        'searchFields' => 'title',
+        'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('events2') . 'Resources/Public/Icons/tx_events2_domain_model_holiday.svg',
+    ),
+    'interface' => array(
+        'showRecordFieldList' => 'title, day, month',
+    ),
+    'columns' => array(
+        'title' => array(
+            'exclude' => 1,
+            'label' => 'LLL:EXT:events2/Resources/Private/Language/locallang_db.xlf:tx_events2_domain_model_holiday.title',
+            'config' => array(
+                'type' => 'input',
+                'size' => 30,
+            ),
+        ),
+        'day' => array(
+            'exclude' => 1,
+            'label' => 'LLL:EXT:events2/Resources/Private/Language/locallang_db.xlf:tx_events2_domain_model_holiday.day',
+            'config' => array(
+                'type' => 'input',
+                'size' => 2,
+            ),
+        ),
+        'month' => array(
+            'exclude' => 1,
+            'label' => 'LLL:EXT:events2/Resources/Private/Language/locallang_db.xlf:tx_events2_domain_model_holiday.month',
+            'config' => array(
+                'type' => 'input',
+                'size' => 2,
+            ),
+        ),
+    ),
+    'types' => array(
+        '1' => array('showitem' => 'title, day, month'),
+    ),
+    'palettes' => array(
+        '1' => array('showitem' => ''),
+    ),
+);
