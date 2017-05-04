983be93cec6b767526d3434685fd89f55d160958
Stefan Froemken <froemken@gmail.com>
2017-05-04 17:04:32 +0200

    Add timestamp argument to activate link in email

--- Classes/Hooks/RealUrlAutoConfiguration.php
+++ Classes/Hooks/RealUrlAutoConfiguration.php
@@ -76,6 +76,12 @@ class RealUrlAutoConfiguration
                     ),
                     'eventDetail' => array(
                         0 => array(
+                            'GETvar' => 'tx_events2_events[timestamp]',
+                            'userFunc' => 'JWeiland\\Events2\\RealUrl\\TimestampMapping->main',
+                            'dateFormat' => 'Y-m-d',
+                            'timeFormat' => 'Hi',
+                        ),
+                        1 => array(
                             'GETvar' => 'tx_events2_events[event]',
                             'lookUpTable' => array(
                                 'table' => 'tx_events2_domain_model_event',
@@ -87,12 +93,6 @@ class RealUrlAutoConfiguration
                                     'spaceCharacter' => '-',
                                 ),
                             ),
-                        ),
-                        1 => array(
-                            'GETvar' => 'tx_events2_events[timestamp]',
-                            'userFunc' => 'JWeiland\\Events2\\RealUrl\\TimestampMapping->main',
-                            'dateFormat' => 'Y-m-d',
-                            'timeFormat' => 'Hi',
                         ),
                     ),
                     'eventLocation' => array(
