43d141feaff1dfd496923dc9b24e77b0e6414e28
Stefan Froemken <froemken@gmail.com>
2018-11-08 08:35:38 +0100

    Remove TypoScript for old TYPO3 versions and switch to new maps2 API

--- Configuration/TCA/Overrides/tx_events2_domain_model_location.php
+++ Configuration/TCA/Overrides/tx_events2_domain_model_location.php
@@ -1,10 +1,18 @@
 <?php
-call_user_func(function() {
-    // Add tx_maps2_uid column to events2 location table
-    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('maps2')) {
-        $result = \JWeiland\Maps2\Tca\Maps2Registry::getInstance()->add(
-            'events2',
-            'tx_events2_domain_model_location'
-        );
-    }
-});
+// Add tx_maps2_uid column to events2 location table
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('maps2')) {
+    \JWeiland\Maps2\Tca\Maps2Registry::getInstance()->add(
+        'events2',
+        'tx_events2_domain_model_location',
+        [
+            'addressColumns' => ['street', 'house_number', 'zip', 'city', 'country'],
+            'countryColumn' => 'country',
+            'synchronizeColumns' => [
+                [
+                    'foreignColumnName' => 'location',
+                    'poiCollectionColumnName' => 'title'
+                ]
+            ]
+        ]
+    );
+}
