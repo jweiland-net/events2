a461ca41bfeb8d3d3795903335ca9719ee515d8c
Stefan Froemken <froemken@gmail.com>
2018-11-08 15:42:34 +0100

    Add compatibility to TYPO3 8 and 9 and set version to 2.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,10 +27,10 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '2.3.1',
+    'version' => '2.4.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.99.99',
+            'typo3' => '8.7.0-9.99.99',
             'static_info_tables' => '6.3.9',
         ],
         'conflicts' => [
