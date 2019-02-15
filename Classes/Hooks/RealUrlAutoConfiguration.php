57f57e9e062037631a894a77985ceb88f679a5c4
Stefan Froemken <froemken@gmail.com>
2019-02-15 11:03:07 +0100

    Use jwtools2 userfunc for better realurl decoding

--- Classes/Hooks/RealUrlAutoConfiguration.php
+++ Classes/Hooks/RealUrlAutoConfiguration.php
@@ -16,6 +16,8 @@ namespace JWeiland\Events2\Hooks;
  */
 use DmitryDulepov\Realurl\Configuration\AutomaticConfigurator;
 use JWeiland\Events2\RealUrl\TimestampMapping;
+use JWeiland\Jwtools2\RealUrl\ConvertTableAliasToId;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 /**
  * Class RealUrl
@@ -28,91 +30,100 @@ class RealUrlAutoConfiguration
      *
      * @param array $parameters
      * @param AutomaticConfigurator $parentObject
-     *
      * @return array Updated configuration
      */
     public function addEvents2Config(array $parameters, AutomaticConfigurator $parentObject)
     {
-        return array_merge_recursive($parameters['config'], [
-            'fileName' => [
-                'defaultToHTMLsuffixOnPrev' => true,
-            ],
-            'postVarSets' => [
-                '_DEFAULT' => [
-                    'event' => [
-                        0 => [
-                            'GETvar' => 'tx_events2_events[controller]',
-                            'valueMap' => [
-                                'd' => 'Day',
-                                'e' => 'Event',
-                                'l' => 'Location',
-                                'v' => 'Video',
-                                'a' => 'Ajax',
+        $realUrlConfiguration = array_merge_recursive(
+            $parameters['config'],
+            [
+                'fileName' => [
+                    'defaultToHTMLsuffixOnPrev' => true,
+                ],
+                'postVarSets' => [
+                    '_DEFAULT' => [
+                        'event' => [
+                            0 => [
+                                'GETvar' => 'tx_events2_events[controller]',
+                                'valueMap' => [
+                                    'd' => 'Day',
+                                    'e' => 'Event',
+                                    'l' => 'Location',
+                                    'v' => 'Video',
+                                    'a' => 'Ajax',
+                                ],
+                                'noMatch' => 'bypass'
                             ],
-                            'noMatch' => 'bypass'
-                        ],
-                        1 => [
-                            'GETvar' => 'tx_events2_events[action]',
-                            'valueMap' => [
-                                'l' => 'list',
-                                'll' => 'listLatest',
-                                'lt' => 'listToday',
-                                'lw' => 'listWeek',
-                                'lr' => 'listRange',
-                                'lsr' => 'listSearchResults',
-                                'lme' => 'listMyEvents',
-                                'n' => 'new',
-                                'c' => 'create',
-                                'e' => 'edit',
-                                'u' => 'update',
-                                'd' => 'delete',
-                                'a' => 'activate',
-                                's' => 'show',
-                                'st' => 'showByTimestamp',
+                            1 => [
+                                'GETvar' => 'tx_events2_events[action]',
+                                'valueMap' => [
+                                    'l' => 'list',
+                                    'll' => 'listLatest',
+                                    'lt' => 'listToday',
+                                    'lw' => 'listWeek',
+                                    'lr' => 'listRange',
+                                    'lsr' => 'listSearchResults',
+                                    'lme' => 'listMyEvents',
+                                    'n' => 'new',
+                                    'c' => 'create',
+                                    'e' => 'edit',
+                                    'u' => 'update',
+                                    'd' => 'delete',
+                                    'a' => 'activate',
+                                    's' => 'show',
+                                    'st' => 'showByTimestamp',
+                                ],
+                                'noMatch' => 'bypass'
                             ],
-                            'noMatch' => 'bypass'
                         ],
-                    ],
-                    'ts' => [
-                        0 => [
-                            'GETvar' => 'tx_events2_events[timestamp]',
-                            'userFunc' => TimestampMapping::class . '->main',
-                            'dateFormat' => 'Y-m-d',
-                            'timeFormat' => 'Hi',
-                        ]
-                    ],
-                    't' => [
-                        0 => [
-                            'GETvar' => 'tx_events2_events[event]',
-                            'lookUpTable' => [
-                                'table' => 'tx_events2_domain_model_event',
-                                'id_field' => 'uid',
-                                'alias_field' => 'CONCAT(title, \'-\', uid)',
-                                'useUniqueCache' => 1,
-                                'useUniqueCache_conf' => [
-                                    'strtolower' => 1,
-                                    'spaceCharacter' => '-',
+                        'ts' => [
+                            0 => [
+                                'GETvar' => 'tx_events2_events[timestamp]',
+                                'userFunc' => TimestampMapping::class . '->main',
+                                'dateFormat' => 'Y-m-d',
+                                'timeFormat' => 'Hi',
+                            ]
+                        ],
+                        't' => [
+                            0 => [
+                                'GETvar' => 'tx_events2_events[event]',
+                                'lookUpTable' => [
+                                    'table' => 'tx_events2_domain_model_event',
+                                    'id_field' => 'uid',
+                                    'alias_field' => 'CONCAT(title, \'-\', uid)',
+                                    'useUniqueCache' => 1,
+                                    'useUniqueCache_conf' => [
+                                        'strtolower' => 1,
+                                        'spaceCharacter' => '-',
+                                    ],
                                 ],
                             ],
                         ],
-                    ],
-                    'eventLocation' => [
-                        0 => [
-                            'GETvar' => 'tx_events2_events[location]',
-                            'lookUpTable' => [
-                                'table' => 'tx_events2_domain_model_location',
-                                'id_field' => 'uid',
-                                'alias_field' => 'CONCAT(location, \'-\', uid)',
-                                'useUniqueCache' => 1,
-                                'useUniqueCache_conf' => [
-                                    'strtolower' => 1,
-                                    'spaceCharacter' => '-',
+                        'eventLocation' => [
+                            0 => [
+                                'GETvar' => 'tx_events2_events[location]',
+                                'lookUpTable' => [
+                                    'table' => 'tx_events2_domain_model_location',
+                                    'id_field' => 'uid',
+                                    'alias_field' => 'CONCAT(location, \'-\', uid)',
+                                    'useUniqueCache' => 1,
+                                    'useUniqueCache_conf' => [
+                                        'strtolower' => 1,
+                                        'spaceCharacter' => '-',
+                                    ],
                                 ],
                             ],
                         ],
                     ],
                 ],
-            ],
-        ]);
+            ]
+        );
+
+        if (ExtensionManagementUtility::isLoaded('jwtools2')) {
+            $userFunc = ConvertTableAliasToId::class . '->convert';
+            $realUrlConfiguration['postVarSets']['_DEFAULT']['t'][0]['userFunc'] = $userFunc;
+        }
+
+        return $realUrlConfiguration;
     }
 }
