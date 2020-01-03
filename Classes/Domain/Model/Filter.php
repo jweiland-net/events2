c36c714cffc31643ffe5b941b15dc249969db12a
Stefan Froemken <froemken@gmail.com>
2020-01-03 09:16:05 +0100

    Allow empty organizer in FE selector

--- Classes/Domain/Model/Filter.php
+++ Classes/Domain/Model/Filter.php
@@ -17,21 +17,33 @@ namespace JWeiland\Events2\Domain\Model;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 
 /**
- * This class will not be persisted. It's a helper to filter organizers by selectbox in frontend.
+ * This class will not be persisted.
+ * It's a helper to filter organizers by selectbox in frontend.
+ *
+ * Hint: After selecting an organizer and switching back to an empty organizer to show all events again
+ * setOrganizer will be called with null.
+ *
+ * @ToDo: Use nullable strict type if we do not support TYPO3 8 anymore because of PHP 7.0
  */
 class Filter extends AbstractEntity
 {
     /**
-     * @var int
+     * @var int|null
      */
-    protected $organizer = 0;
+    protected $organizer;
 
-    public function getOrganizer(): int
+    /**
+     * @return int|null
+     */
+    public function getOrganizer()
     {
         return $this->organizer;
     }
 
-    public function setOrganizer(int $organizer)
+    /**
+     * @param int|null $organizer
+     */
+    public function setOrganizer($organizer = null)
     {
         $this->organizer = $organizer;
     }
