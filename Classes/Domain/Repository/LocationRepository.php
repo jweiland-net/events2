15dfa3ba26664ff8142a6bc2fa0bfc0edfb2c5a4
Stefan Froemken <froemken@gmail.com>
2020-08-06 17:15:49 +0200

    Move findLocations method into LocationsRepository

--- Classes/Domain/Repository/LocationRepository.php
+++ Classes/Domain/Repository/LocationRepository.php
@@ -11,6 +11,8 @@ declare(strict_types=1);
 
 namespace JWeiland\Events2\Domain\Repository;
 
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
@@ -26,4 +28,35 @@ class LocationRepository extends Repository
     protected $defaultOrderings = [
         'location' => QueryInterface::ORDER_ASCENDING,
     ];
+
+    /**
+     * This method does not use any Extbase Queries, as it was needed by Ajax Request FindLocations
+     * which does not have any Extbase Context.
+     *
+     * @param string $locationPart
+     * @return array
+     */
+    public function findLocations(string $locationPart): array
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable('tx_events2_domain_model_location');
+
+        $locations = $queryBuilder
+            ->select('uid', 'location as label')
+            ->from('tx_events2_domain_model_location')
+            ->where(
+                $queryBuilder->expr()->like(
+                    'location',
+                    $queryBuilder->createNamedParameter('%' . $locationPart . '%', \PDO::PARAM_STR)
+                )
+            )
+            ->orderBy('location', 'ASC')
+            ->execute()
+            ->fetchAll();
+
+        if (empty($locations)) {
+            $locations = [];
+        }
+        return $locations;
+    }
 }
