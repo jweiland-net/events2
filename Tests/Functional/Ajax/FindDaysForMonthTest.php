c490d0c0cfa432cd8cf09f049225060d96b98ed5
Stefan Froemken <froemken@gmail.com>
2020-08-07 09:43:05 +0200

    Repair functionalTests for Ajax Requests

--- Tests/Functional/Ajax/FindDaysForMonthTest.php
+++ Tests/Functional/Ajax/FindDaysForMonthTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace JWeiland\Events2\Tests\Unit\Ajax;
+namespace JWeiland\Events2\Tests\Functional\Ajax;
 
 /*
  * This file is part of the events2 project.
@@ -18,9 +18,9 @@ namespace JWeiland\Events2\Tests\Unit\Ajax;
 use JWeiland\Events2\Ajax\FindDaysForMonth;
 use JWeiland\Events2\Configuration\ExtConf;
 use JWeiland\Events2\Service\DatabaseService;
-use JWeiland\Events2\Tests\Unit\AbstractUnitTestCase;
 use JWeiland\Events2\Tests\Unit\Utility\AccessibleProxies\ExtensionManagementUtilityAccessibleProxy;
 use JWeiland\Events2\Utility\DateTimeUtility;
+use Nimut\TestingFramework\TestCase\FunctionalTestCase;
 use Prophecy\Argument;
 use Prophecy\Prophecy\ObjectProphecy;
 use TYPO3\CMS\Core\Cache\CacheManager;
@@ -34,7 +34,7 @@ use TYPO3\CMS\Frontend\Page\CacheHashCalculator;
 /**
  * Test case.
  */
-class FindDaysForMonthTest extends AbstractUnitTestCase
+class FindDaysForMonthTest extends FunctionalTestCase
 {
     /**
      * @var FindDaysForMonth
@@ -42,9 +42,9 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
     protected $subject;
 
     /**
-     * @var ExtConf|ObjectProphecy
+     * @var ExtConf
      */
-    protected $extConfProphecy;
+    protected $extConf;
 
     /**
      * @var FrontendUserAuthentication|ObjectProphecy
@@ -62,46 +62,34 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
     protected $cacheManagerProphecy;
 
     /**
-     * set up.
+     * @var array
      */
+    protected $testExtensionsToLoad = [
+        'typo3conf/ext/events2'
+    ];
+
     public function setUp()
     {
-        $this->extConfProphecy = $this->prophesize(ExtConf::class);
-        $this->extConfProphecy->getRecurringPast()->willReturn(3);
-        $this->extConfProphecy->getRecurringFuture()->willReturn(6);
+        parent::setUp();
+
+        $this->extConf = new ExtConf();
+        $this->extConf->setRecurringPast(3);
+        $this->extConf->setRecurringFuture(6);
 
         $this->frontendUserAuthenticationProphecy = $this->prophesize(FrontendUserAuthentication::class);
 
-        $cacheData = [
-            'categoryRegistry' => serialize(new CategoryRegistry())
-        ];
-
-        $this->phpFrontendProphecy = $this->prophesize(PhpFrontend::class);
-        $this->phpFrontendProphecy->require(Argument::any())->shouldBeCalled()->willReturn($cacheData);
-
-        /** @var CacheManager|ObjectProphecy $cacheManagerProphecy */
-        $this->cacheManagerProphecy = $this->prophesize(CacheManager::class);
-        $this->cacheManagerProphecy
-            ->getCache('cache_core')
-            ->shouldBeCalled()
-            ->willReturn($this->phpFrontendProphecy->reveal());
-        GeneralUtility::setSingletonInstance(CacheManager::class, $this->cacheManagerProphecy->reveal());
-
         $this->subject = new FindDaysForMonth(
-            $this->extConfProphecy->reveal(),
+            $this->extConf,
             new DateTimeUtility(),
             new CacheHashCalculator()
         );
     }
 
-    /**
-     * tear down.
-     */
     public function tearDown()
     {
-        parent::tearDown();
         ExtensionManagementUtilityAccessibleProxy::setCacheManager(null);
         unset($this->subject);
+        parent::tearDown();
     }
 
     /**
@@ -112,10 +100,66 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
     public function dataProviderForProcessAjaxRequestForcesTooHighMonthAndYearInRange()
     {
         $arguments = [];
-        $arguments['negative values'] = ['http://www.example.com/?tx_events2_events[arguments][month]=-50&tx_events2_events[arguments][year]=-2400', ['month' => '01', 'year' => '1500'], 1];
-        $arguments['valid int values'] = ['http://www.example.com/?tx_events2_events[arguments][month]=10&tx_events2_events[arguments][year]=2100', ['month' => '10', 'year' => '2100'], 10];
-        $arguments['too huge values'] = ['http://www.example.com/?tx_events2_events[arguments][month]=250&tx_events2_events[arguments][year]=12050', ['month' => '12', 'year' => '2500'], 12];
-        $arguments['add 0 to small numbers'] = ['http://www.example.com/?tx_events2_events[arguments][month]=3&tx_events2_events[arguments][year]=2145', ['month' => '03', 'year' => '2145'], 3];
+        $arguments['negative values'] = [
+            [
+                'tx_events2_events' => [
+                    'arguments' => [
+                        'month' => '-50',
+                        'year' => '-2400',
+                    ]
+                ]
+            ],
+            [
+                'month' => '01',
+                'year' => '1500'
+            ],
+            1
+        ];
+        $arguments['valid int values'] = [
+            [
+                'tx_events2_events' => [
+                    'arguments' => [
+                        'month' => '10',
+                        'year' => '2100',
+                    ]
+                ]
+            ],
+            [
+                'month' => '10',
+                'year' => '2100'
+            ],
+            10
+        ];
+        $arguments['too huge values'] = [
+            [
+                'tx_events2_events' => [
+                    'arguments' => [
+                        'month' => '250',
+                        'year' => '12050',
+                    ]
+                ]
+            ],
+            [
+                'month' => '12',
+                'year' => '2500'
+            ],
+            12
+        ];
+        $arguments['add 0 to small numbers'] = [
+            [
+                'tx_events2_events' => [
+                    'arguments' => [
+                        'month' => '3',
+                        'year' => '2145',
+                    ]
+                ]
+            ],
+            [
+                'month' => '03',
+                'year' => '2145'
+            ],
+            3
+        ];
 
         return $arguments;
     }
@@ -124,7 +168,7 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
      * @test
      * @dataProvider dataProviderForProcessAjaxRequestForcesTooHighMonthAndYearInRange
      */
-    public function processAjaxRequestForcesTooHighMonthAndYearInRange($uri, $expectedArguments, $expectedMonth)
+    public function processAjaxRequestForcesTooHighMonthAndYearInRange($queryParams, $expectedArguments, $expectedMonth)
     {
         /** @var FrontendUserAuthentication|ObjectProphecy $frontendUserAuthentication */
         $frontendUserAuthentication = $this->prophesize(FrontendUserAuthentication::class);
@@ -139,19 +183,9 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
 
         GeneralUtility::addInstance(FrontendUserAuthentication::class, $frontendUserAuthentication->reveal());
 
-        $this->buildAssertionForDatabaseWithReturnValue(
-            'tx_events2_domain_model_holiday',
-            [],
-            [
-                [
-                    'expr' => 'eq',
-                    'field' => 'month',
-                    'value' => $expectedMonth
-                ]
-            ]
-        );
-
-        $this->subject->processRequest(new ServerRequest($uri));
+        $request = new ServerRequest('http://www.example.com/');
+        $request = $request->withQueryParams($queryParams);
+        $this->subject->processRequest($request);
     }
 
     /**
@@ -160,12 +194,15 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
     public function processAjaxRequestWillFindDaysInRangeOfFirstAndLastDateOfMonth()
     {
         $currentDate = new \DateTime('now midnight');
-        $uri = sprintf(
-            'http://www.example.com/?tx_events2_events[arguments][pidOfListPage]=%d&tx_events2_events[arguments][month]=%d&tx_events2_events[arguments][year]=%d',
-            4321,
-            (int)$currentDate->format('n'),
-            (int)$currentDate->format('Y')
-        );
+        $queryParams = [
+            'tx_events2_events' => [
+                'arguments' => [
+                    'pidOfListPage' => 4321,
+                    'month' => (int)$currentDate->format('n'),
+                    'year' => (int)$currentDate->format('Y'),
+                ]
+            ]
+        ];
         $startDate = clone $currentDate;
         $startDate->modify('first day of this month');
         $endDate = clone $currentDate;
@@ -182,19 +219,9 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
         $databaseServiceProphecy->getDaysInRange($startDate, $endDate, [], [])->shouldBeCalled()->willReturn([]);
         GeneralUtility::addInstance(DatabaseService::class, $databaseServiceProphecy->reveal());
 
-        $this->buildAssertionForDatabaseWithReturnValue(
-            'tx_events2_domain_model_holiday',
-            [],
-            [
-                [
-                    'expr' => 'eq',
-                    'field' => 'month',
-                    'value' => (int)$currentDate->format('n')
-                ]
-            ]
-        );
-
-        $this->subject->processRequest(new ServerRequest($uri));
+        $request = new ServerRequest('http://www.example.com/');
+        $request = $request->withQueryParams($queryParams);
+        $this->subject->processRequest($request);
     }
 
     /**
@@ -203,13 +230,16 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
     public function processAjaxRequestWillFindDaysInRangeOfFirstAndLastDateOfMonthWithStoragePids()
     {
         $currentDate = new \DateTime('now midnight');
-        $uri = sprintf(
-            'http://www.example.com/?tx_events2_events[arguments][storagePids]=%s&tx_events2_events[arguments][pidOfListPage]=%d&tx_events2_events[arguments][month]=%d&tx_events2_events[arguments][year]=%d',
-            '21,22,23',
-            4321,
-            (int)$currentDate->format('n'),
-            (int)$currentDate->format('Y')
-        );
+        $queryParams = [
+            'tx_events2_events' => [
+                'arguments' => [
+                    'storagePids' => '21,22,23',
+                    'pidOfListPage' => 4321,
+                    'month' => (int)$currentDate->format('n'),
+                    'year' => (int)$currentDate->format('Y'),
+                ]
+            ]
+        ];
         $startDate = clone $currentDate;
         $startDate->modify('first day of this month');
         $endDate = clone $currentDate;
@@ -226,19 +256,9 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
         $databaseServiceProphecy->getDaysInRange($startDate, $endDate, [21, 22, 23], [])->shouldBeCalled()->willReturn([]);
         GeneralUtility::addInstance(DatabaseService::class, $databaseServiceProphecy->reveal());
 
-        $this->buildAssertionForDatabaseWithReturnValue(
-            'tx_events2_domain_model_holiday',
-            [],
-            [
-                [
-                    'expr' => 'eq',
-                    'field' => 'month',
-                    'value' => (int)$currentDate->format('n')
-                ]
-            ]
-        );
-
-        $this->subject->processRequest(new ServerRequest($uri));
+        $request = new ServerRequest('http://www.example.com/');
+        $request = $request->withQueryParams($queryParams);
+        $this->subject->processRequest($request);
     }
 
     /**
@@ -247,14 +267,17 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
     public function processAjaxRequestWillFindDaysInRangeOfFirstAndLastDateOfMonthWithStoragePidsAndCategories()
     {
         $currentDate = new \DateTime('now midnight');
-        $uri = sprintf(
-            'http://www.example.com/?tx_events2_events[arguments][categories]=%s&tx_events2_events[arguments][storagePids]=%s&tx_events2_events[arguments][pidOfListPage]=%d&tx_events2_events[arguments][month]=%d&tx_events2_events[arguments][year]=%d',
-            '31,32,33',
-            '21,22,23',
-            4321,
-            (int)$currentDate->format('n'),
-            (int)$currentDate->format('Y')
-        );
+        $queryParams = [
+            'tx_events2_events' => [
+                'arguments' => [
+                    'categories' => '31,32,33',
+                    'storagePids' => '21,22,23',
+                    'pidOfListPage' => 4321,
+                    'month' => (int)$currentDate->format('n'),
+                    'year' => (int)$currentDate->format('Y'),
+                ]
+            ]
+        ];
         $startDate = clone $currentDate;
         $startDate->modify('first day of this month');
         $endDate = clone $currentDate;
@@ -271,19 +294,9 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
         $databaseServiceProphecy->getDaysInRange($startDate, $endDate, [21, 22, 23], [31, 32, 33])->shouldBeCalled()->willReturn([]);
         GeneralUtility::addInstance(DatabaseService::class, $databaseServiceProphecy->reveal());
 
-        $this->buildAssertionForDatabaseWithReturnValue(
-            'tx_events2_domain_model_holiday',
-            [],
-            [
-                [
-                    'expr' => 'eq',
-                    'field' => 'month',
-                    'value' => (int)$currentDate->format('n')
-                ]
-            ]
-        );
-
-        $this->subject->processRequest(new ServerRequest($uri));
+        $request = new ServerRequest('http://www.example.com/');
+        $request = $request->withQueryParams($queryParams);
+        $this->subject->processRequest($request);
     }
 
     /**
@@ -292,12 +305,15 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
     public function processAjaxRequestWillFindDaysInRangeOfEarliestDateAndLastDateOfMonth()
     {
         $currentDate = new \DateTime('-3 months midnight');
-        $uri = sprintf(
-            'http://www.example.com/?tx_events2_events[arguments][pidOfListPage]=%d&tx_events2_events[arguments][month]=%d&tx_events2_events[arguments][year]=%d',
-            4321,
-            (int)$currentDate->format('n'),
-            (int)$currentDate->format('Y')
-        );
+        $queryParams = [
+            'tx_events2_events' => [
+                'arguments' => [
+                    'pidOfListPage' => 4321,
+                    'month' => (int)$currentDate->format('n'),
+                    'year' => (int)$currentDate->format('Y'),
+                ]
+            ]
+        ];
         $startDate = clone $currentDate;
         $endDate = clone $currentDate;
         $endDate->modify('last day of this month')->modify('tomorrow');
@@ -313,19 +329,9 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
         $databaseServiceProphecy->getDaysInRange($startDate, $endDate, [], [])->shouldBeCalled()->willReturn([]);
         GeneralUtility::addInstance(DatabaseService::class, $databaseServiceProphecy->reveal());
 
-        $this->buildAssertionForDatabaseWithReturnValue(
-            'tx_events2_domain_model_holiday',
-            [],
-            [
-                [
-                    'expr' => 'eq',
-                    'field' => 'month',
-                    'value' => (int)$currentDate->format('n')
-                ]
-            ]
-        );
-
-        $this->subject->processRequest(new ServerRequest($uri));
+        $request = new ServerRequest('http://www.example.com/');
+        $request = $request->withQueryParams($queryParams);
+        $this->subject->processRequest($request);
     }
 
     /**
@@ -334,12 +340,15 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
     public function processAjaxRequestWillFindDaysInRangeOfFirstDateAndLatestDateOfMonth()
     {
         $currentDate = new \DateTime('+6 months midnight');
-        $uri = sprintf(
-            'http://www.example.com/?tx_events2_events[arguments][pidOfListPage]=%d&tx_events2_events[arguments][month]=%d&tx_events2_events[arguments][year]=%d',
-            4321,
-            (int)$currentDate->format('n'),
-            (int)$currentDate->format('Y')
-        );
+        $queryParams = [
+            'tx_events2_events' => [
+                'arguments' => [
+                    'pidOfListPage' => 4321,
+                    'month' => (int)$currentDate->format('n'),
+                    'year' => (int)$currentDate->format('Y'),
+                ]
+            ]
+        ];
         $startDate = clone $currentDate;
         $startDate->modify('first day of this month');
         $endDate = clone $currentDate;
@@ -356,18 +365,8 @@ class FindDaysForMonthTest extends AbstractUnitTestCase
         $databaseServiceProphecy->getDaysInRange($startDate, $endDate, [], [])->shouldBeCalled()->willReturn([]);
         GeneralUtility::addInstance(DatabaseService::class, $databaseServiceProphecy->reveal());
 
-        $this->buildAssertionForDatabaseWithReturnValue(
-            'tx_events2_domain_model_holiday',
-            [],
-            [
-                [
-                    'expr' => 'eq',
-                    'field' => 'month',
-                    'value' => (int)$currentDate->format('n')
-                ]
-            ]
-        );
-
-        $this->subject->processRequest(new ServerRequest($uri));
+        $request = new ServerRequest('http://www.example.com/');
+        $request = $request->withQueryParams($queryParams);
+        $this->subject->processRequest($request);
     }
 }
