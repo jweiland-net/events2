49d8db0688e6b2accd2b3272654976df1852ef8a
Stefan Froemken <froemken@gmail.com>
2020-08-27 16:42:07 +0200

    Transfer TS setting to flexform, if not set

--- Classes/Service/TypoScriptService.php
+++ Classes/Service/TypoScriptService.php
@@ -20,13 +20,12 @@ class TypoScriptService
     public function override(array &$mergedFlexFormSettings, array $typoScriptSettings): void
     {
         foreach ($typoScriptSettings as $property => $value) {
-            if (isset($typoScriptSettings[$property]) && is_array($typoScriptSettings[$property])) {
-                if (is_array($typoScriptSettings[$property])) {
-                    $this->override($mergedFlexFormSettings[$property], $typoScriptSettings[$property]);
-                }
+            if (is_array($typoScriptSettings[$property]) && is_array($mergedFlexFormSettings[$property])) {
+                $this->override($mergedFlexFormSettings[$property], $typoScriptSettings[$property]);
             } elseif (
-                $mergedFlexFormSettings[$property] === '0' ||
-                (
+                $mergedFlexFormSettings[$property] === '0'
+                || !isset($mergedFlexFormSettings[$property])
+                || (
                     is_string($mergedFlexFormSettings[$property]) &&
                     strlen($mergedFlexFormSettings[$property]) === 0
                 )
