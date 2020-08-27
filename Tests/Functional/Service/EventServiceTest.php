4066d6420a34015285229a1d538d1fe3d4f52913
Stefan Froemken <froemken@gmail.com>
2020-08-27 17:03:43 +0200

    Move EventServiceTest to functional tests

--- Tests/Functional/Service/EventServiceTest.php
+++ Tests/Functional/Service/EventServiceTest.php
@@ -17,37 +17,41 @@ use JWeiland\Events2\Domain\Model\Time;
 use JWeiland\Events2\Domain\Repository\EventRepository;
 use JWeiland\Events2\Service\EventService;
 use JWeiland\Events2\Utility\DateTimeUtility;
-use Nimut\TestingFramework\TestCase\UnitTestCase;
+use Nimut\TestingFramework\TestCase\FunctionalTestCase;
 use Prophecy\Prophecy\ObjectProphecy;
 use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
 /**
  * Test case for class \JWeiland\Events2\Service\EventService
  */
-class EventServiceTest extends UnitTestCase
+class EventServiceTest extends FunctionalTestCase
 {
     /**
      * @var EventService
      */
     protected $subject;
 
-    /**
-     * set up.
-     */
+    protected $testExtensionsToLoad = [
+        'typo3conf/ext/events2'
+    ];
+
     public function setUp()
     {
+        parent::setUp();
+
         $this->subject = new EventService(
             new ExtConf(),
             new DateTimeUtility()
         );
     }
 
-    /**
-     * tear down.
-     */
     public function tearDown()
     {
-        unset($this->subject);
+        unset(
+            $this->subject
+        );
+
+        parent::tearDown();
     }
 
     /**
