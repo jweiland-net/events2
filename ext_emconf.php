666f5a98a6f44e5873d8c2690afde1881174d38e
Stefan Froemken <froemken@gmail.com>
2021-06-09 14:57:50 +0200

    Remove TYPO3 9 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '6.3.0',
+    'version' => '7.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.20-10.4.99',
+            'typo3' => '10.4.14-10.4.99',
             'static_info_tables' => '6.9.0',
         ],
         'conflicts' => [
