60f15c1d014a99b7c44fb7cd2483c57b5691a3ac
Stefan Froemken <froemken@gmail.com>
2021-06-15 09:26:04 +0200

    Move a lot of logic of DayController into EventListeners

---
+++ Classes/EventListener/AddOrganizersForFilterEventListener.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/events2.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Events2\EventListener;
+
+use JWeiland\Events2\Domain\Repository\OrganizerRepository;
+use JWeiland\Events2\Event\PostProcessFluidVariablesEvent;
+
+/**
+ * Add organizer for selector in list views.
+ * Selector will only be shown if there are more than 1 organizers assigned
+ */
+class AddOrganizersForFilterEventListener extends AbstractControllerEventListener
+{
+    /**
+     * @var OrganizerRepository
+     */
+    protected $organizerRepository;
+
+    protected $allowedControllerActions = [
+        'Day' => [
+            'list',
+            'listLatest',
+            'listToday',
+            'listThisWeek',
+            'listRange',
+        ]
+    ];
+
+    public function __construct(OrganizerRepository $organizerRepository)
+    {
+        $this->organizerRepository = $organizerRepository;
+    }
+
+    public function __invoke(PostProcessFluidVariablesEvent $event): void
+    {
+        if (
+            $this->isValidRequest($event)
+            && array_key_exists('showFilterForOrganizerInFrontend', $event->getSettings())
+            && $event->getSettings()['showFilterForOrganizerInFrontend'] === '1'
+        ) {
+            $event->addFluidVariable(
+                'organizers',
+                $this->organizerRepository->getOrganizersForFilter()
+            );
+        }
+    }
+}
