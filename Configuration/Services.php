f7048c6d394d69eb888e811be3f449ac26fd85fb
Stefan Froemken <froemken@gmail.com>
2022-11-25 14:43:33 +0100

    Use interface_exists insteadof class_exists in Services.php

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -13,7 +13,7 @@ return static function (
     // As IndexerHook.php is incompatible with an interface of solr 11.2 we have to exclude both files
     // from "resource" in Services.yaml and load/configure the file individual here.
     // We can't use EMU::isLoaded() as PackageManager is not loaded until now. Using class_exists()
-    if (class_exists(\ApacheSolrForTypo3\Solr\IndexQueue\PageIndexerDocumentsModifier::class)) {
+    if (interface_exists(\ApacheSolrForTypo3\Solr\IndexQueue\PageIndexerDocumentsModifier::class)) {
         if (version_compare($typo3Version->getBranch(), '11.4', '>=')) {
             $indexerHookClassName = \JWeiland\Events2\Hooks\Solr\IndexerHook::class;
         } else {
