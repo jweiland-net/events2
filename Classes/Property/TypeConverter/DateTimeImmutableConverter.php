2d88f4fe4ca2aa9c308c2e6368b8452c12eecf82
Stefan Froemken <sfroemken@jweiland.net>
2023-10-16 09:37:17 +0200

    Migrate TypeConverter to Services.yaml

--- Classes/Property/TypeConverter/DateTimeImmutableConverter.php
+++ Classes/Property/TypeConverter/DateTimeImmutableConverter.php
@@ -74,11 +74,13 @@ class DateTimeImmutableConverter extends AbstractTypeConverter
 
     /**
      * @var string[]
+     * @deprecated will be removed in TYPO3 v13.0, as this is defined in Services.yaml.
      */
     protected $sourceTypes = ['string', 'integer', 'array'];
 
     /**
      * @var string
+     * @deprecated will be removed in TYPO3 v13.0, as this is defined in Services.yaml.
      */
     protected $targetType = \DateTimeImmutable::class;
 
@@ -87,6 +89,7 @@ class DateTimeImmutableConverter extends AbstractTypeConverter
      * in future the version of TYPO3 has priority.
      *
      * @var int
+     * @deprecated will be removed in TYPO3 v13.0, as this is defined in Services.yaml.
      */
     protected $priority = 1;
 
