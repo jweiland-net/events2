7de44c4b05ddd34d55352f4bc3951ef95466846b
Stefan Froemken <sfroemken@jweiland.net>
2024-02-07 11:31:34 +0100

    Keep hidden col of event and days in sync

---
+++ Classes/EventListener/ActivateHiddenDayRecordsEventListener.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/events2.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Events2\EventListener;
+
+use JWeiland\Events2\Domain\Model\Event;
+use JWeiland\Events2\Event\PostProcessFluidVariablesEvent;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+
+/*
+ * After activating an event via the link of activation mail (frontend created events) just the event itself
+ * will be activated while the day records are still hidden. We can't loop through $event->getDays() as at that point
+ * the day records are still hidden and are not found by the internals of DayRepository.
+ *
+ * This EventListener will find these hidden day records and activates them.
+ */
+class ActivateHiddenDayRecordsEventListener extends AbstractControllerEventListener
+{
+    protected array $allowedControllerActions = [
+        'Management' => [
+            'activate',
+        ],
+    ];
+
+    public function __construct(readonly private ConnectionPool $connectionPool)
+    {
+    }
+
+    public function __invoke(PostProcessFluidVariablesEvent $controllerActionEvent): void
+    {
+        if (!$this->isValidRequest($controllerActionEvent)) {
+            return;
+        }
+
+        $event = $controllerActionEvent->getFluidVariables()['event'] ?? null;
+        if (!$event instanceof Event) {
+            return;
+        }
+
+        $this->getConnectionForDay()->update(
+            'tx_events2_domain_model_day',
+            [
+                'hidden' => 0,
+            ],
+            [
+                'event' => $event->getUid(),
+            ],
+            [
+                Connection::PARAM_INT,
+            ]
+        );
+    }
+
+    private function getConnectionForDay(): Connection
+    {
+        return $this->connectionPool->getConnectionForTable('tx_events2_domain_model_day');
+    }
+}
