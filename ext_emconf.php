e0cc6dc955a6d7c103c890b0ef4be70243774e02
Stefan Froemken <sfroemken@jweiland.net>
2024-02-07 15:51:53 +0100

    Update to version 9.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,11 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '8.3.5',
+    'version' => '9.0.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.99.99',
-            'typo3' => '12.4.5-12.4.99',
+            'typo3' => '12.4.8-12.4.99',
         ],
         'conflicts' => [
         ],
