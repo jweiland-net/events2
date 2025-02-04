e101ca2d46d676baaad3822faac3d02ff35ab7fa
Stefan Froemken <sfroemken@jweiland.net>
2025-02-04 16:18:50 +0100

    Use category UID from sys_category_record_mm instead of sys_reaction

--- Classes/Reaction/ImportEventsReaction.php
+++ Classes/Reaction/ImportEventsReaction.php
@@ -11,12 +11,17 @@ declare(strict_types=1);
 
 namespace JWeiland\Events2\Reaction;
 
+use Doctrine\DBAL\Exception;
 use JWeiland\Events2\Configuration\ImportConfiguration;
 use JWeiland\Events2\Importer\JsonImporter;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\Http\ResponseFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Reactions\Model\ReactionInstruction;
 use TYPO3\CMS\Reactions\Reaction\ReactionInterface;
 
@@ -28,6 +33,7 @@ class ImportEventsReaction implements ReactionInterface
     public function __construct(
         private readonly JsonImporter $jsonImporter,
         private readonly ResponseFactory $responseFactory,
+        private readonly QueryBuilder $queryBuilder,
         private readonly StreamFactoryInterface $streamFactory,
     ) {}
 
@@ -49,7 +55,9 @@ class ImportEventsReaction implements ReactionInterface
     public function react(ServerRequestInterface $request, array $payload, ReactionInstruction $reaction): ResponseInterface
     {
         $statusData = [];
-        $statusData['success'] = $this->jsonImporter->import(new ImportConfiguration($payload, $reaction));
+        $statusData['success'] = $this->jsonImporter->import(
+            new ImportConfiguration($payload, $reaction, $this->getParentCategory($reaction))
+        );
 
         if ($statusData['success'] === false) {
             $statusData['error'] = 'Error while importing events';
@@ -58,6 +66,41 @@ class ImportEventsReaction implements ReactionInterface
         }
 
         return $this->jsonResponse($statusData);
+    }
+
+    protected function getParentCategory(ReactionInstruction $reaction): int
+    {
+        $queryBuilder = $this->queryBuilder;
+        $queryBuilder
+            ->getRestrictions()
+            ->removeAll()
+            ->add(GeneralUtility::makeInstance(DeletedRestriction::class));
+
+        try {
+            $categoryMmRecord = $queryBuilder
+                ->select('uid_local')
+                ->from('sys_category_record_mm')
+                ->where(
+                    $queryBuilder->expr()->eq(
+                        'uid_foreign',
+                        $queryBuilder->createNamedParameter($reaction->getUid(), Connection::PARAM_INT)
+                    ),
+                    $queryBuilder->expr()->eq(
+                        'fieldname',
+                        $queryBuilder->createNamedParameter('parent_category')
+                    ),
+                    $queryBuilder->expr()->eq(
+                        'tablenames',
+                        $queryBuilder->createNamedParameter('sys_reaction')
+                    ),
+                )
+                ->executeQuery()
+                ->fetchAssociative();
+            return (int)(is_array($categoryMmRecord) ? $categoryMmRecord['uid_local'] : 0);
+        } catch (Exception) {
+        }
+
+        return 0;
     }
 
     protected function jsonResponse(array $data, int $statusCode = 200): ResponseInterface
