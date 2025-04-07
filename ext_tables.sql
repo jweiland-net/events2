31d19ad5664904007608ab6c5e3970a03ed6e0c0
Stefan Froemken <sfroemken@jweiland.net>
2025-04-07 14:32:11 +0200

    Add INDEX for exceptions and time type

--- ext_tables.sql
+++ ext_tables.sql
@@ -64,7 +64,8 @@ CREATE TABLE tx_events2_domain_model_time
 	event      int(11) unsigned DEFAULT '0' NOT NULL,
 	exception  int(11) unsigned DEFAULT '0' NOT NULL,
 
-	KEY        eventType (event,type)
+	KEY        eventType (event,type),
+	KEY        exceptionType (exception,type)
 );
 
 #
