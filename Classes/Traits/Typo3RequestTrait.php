3dd1c96379503de4fb068913addb797ab4e504f4
Stefan Froemken <sfroemken@jweiland.net>
2025-10-01 09:29:46 +0200

    Apply CGL

--- Classes/Traits/Typo3RequestTrait.php
+++ Classes/Traits/Typo3RequestTrait.php
@@ -14,7 +14,6 @@ namespace JWeiland\Events2\Traits;
 use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Core\Http\ApplicationType;
 use TYPO3\CMS\Core\Http\ServerRequest;
-use TYPO3\CMS\Core\Http\ServerRequestFactory;
 use TYPO3\CMS\Core\Utility\ArrayUtility;
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
