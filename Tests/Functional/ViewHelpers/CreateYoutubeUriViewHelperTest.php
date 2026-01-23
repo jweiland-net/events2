1b5d37c3a501025c314f4145a0d344c3d2cc593f
Stefan Froemken <sfroemken@jweiland.net>
2026-01-23 15:04:06 +0100

    Apply CGL

--- Tests/Functional/ViewHelpers/CreateYoutubeUriViewHelperTest.php
+++ Tests/Functional/ViewHelpers/CreateYoutubeUriViewHelperTest.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the package jweiland/events2.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace JWeiland\Events2\Tests\Functional\ViewHelpers;
 
 use PHPUnit\Framework\Attributes\DataProvider;
